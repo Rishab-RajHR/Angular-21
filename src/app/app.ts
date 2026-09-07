e0ad9fde5efea7af0ee268f8fd922c20018f1db2
Rishab Raj <rr0948555@gmail.com>
2026-09-07 14:13:49 +0530

    Components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Login } from './login/login';
 
 @Component({
-  imports: [RouterOutlet],
   selector: 'app-root',
+  imports: [Login],
   styleUrl: './app.css',
   templateUrl: './app.html',
 })
