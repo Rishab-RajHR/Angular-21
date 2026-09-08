3c0745ee756285c76edc860bf400c25c7231fee9
Rishab Raj <rr0948555@gmail.com>
2026-09-08 22:54:40 +0530

    ForLoop

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,24 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
-  imports: [RouterOutlet],
+  imports: [],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
 })
 export class App {
-  protected readonly title = signal('angular-tut');
-  name = "Angular Tutorial";
-  username = "TovinoThomas";
-   isAdmin = true;
-   count = 3;
+  //  items  = ['HTML', 'CSS', 'JavaScript', 'Angular' ];
+
+  //  users = [
+  //    { name: 'Mohit', age: 24 },
+  //    { name: 'John', age: 30 },
+  //    { name: 'Alex', age: 34 }
+  //  ];
+
+   items = ['Apple', 'Banana', 'Cherry', 'Dates'];
+
+   removeItem(i: number){
+       this.items.splice(i, 1);
+   }
+
 }
