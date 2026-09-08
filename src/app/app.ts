08bafec18ea583e50039aa1912d0fe1c8b2c916e
Rishab Raj <rr0948555@gmail.com>
2026-09-08 13:08:31 +0530

    Switch Statement

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,18 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
-  imports: [RouterOutlet],
+  imports: [],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
 })
 export class App {
-  protected readonly title = signal('angular-tut');
-  name = "Angular Tutorial";
-  username = "TovinoThomas";
-   isAdmin = true;
-   count = 3;
+  value: string = 'home';
+
+  tab='home';
+
+  categories = '';
+  updateCat(val: string) {
+      this.categories = val.toLowerCase();
+  }
 }
