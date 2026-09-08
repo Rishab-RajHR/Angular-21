c7e89ea5f1b21d9d5aa92ac32e94d1e2e2193c50
Rishab Raj <rr0948555@gmail.com>
2026-09-08 10:24:54 +0530

    Get & Set

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,22 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
 
 @Component({
-  imports: [RouterOutlet],
+  imports: [],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
 })
 export class App {
-  protected readonly title = signal('angular-tut');
-  name = "Angular Tutorial";
-  username = "TovinoThomas";
-   isAdmin = true;
-   count = 3;
+    name: string = "";
+    city: string = "";
+    email: string = "";
+
+    updateName(val:string){
+       this.name = val;
+    }
+
+    getEmail(val:string){
+        this.email = val;
+    }
 }
