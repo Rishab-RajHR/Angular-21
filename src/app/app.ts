0811aedf8324fbeb62ba1bbb1bd132f8854405e1
Rishab Raj <rr0948555@gmail.com>
2026-09-09 18:46:14 +0530

    Signals Deep Dive

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,35 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, computed, effect, signal } from '@angular/core';
 
 @Component({
-  imports: [RouterOutlet],
+  imports: [],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
 })
 export class App {
-  protected readonly title = signal('angular-tut');
-  name = "Angular Tutorial";
-  username = "TovinoThomas";
-   isAdmin = true;
-   count = 3;
+  //  count = signal(0);
+  //  name = signal('Angular');
+
+  //  setCount(){
+  //     this.count.set(2);
+  //  }
+
+  count = signal(2);
+  price = signal(5);
+  // doubleCount = computed(() => this.count() * 2);
+
+  total = computed(() => this.count() * this.price());
+  constructor(){
+      effect(() => {
+          console.log("Total:", this.total());
+      })
+  }
+
+   updateCount() {
+      this.count.update(c => c + 1);
+   }
+
+   updatePrice(){
+     this.price.update(p => p + 1);
+   }
 }
