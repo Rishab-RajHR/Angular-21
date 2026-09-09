25b5ec0fe96d3df7d27c8f26af7aefbd1b2eddba
Rishab Raj <rr0948555@gmail.com>
2026-09-09 12:12:42 +0530

    Signals Data Type

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,32 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
+interface User {
+    name : string;
+    age : number;
+}
 
 @Component({
-  imports: [RouterOutlet],
+  imports: [],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
 })
 export class App {
-  protected readonly title = signal('angular-tut');
-  name = "Angular Tutorial";
-  username = "TovinoThomas";
-   isAdmin = true;
-   count = 3;
+    count = signal<number>(0);
+    name = signal<string>('Angular');
+    isAdmin = signal<boolean>(true);
+
+    numbers = signal<number[]>([1,20,25,30]);
+    users = signal<User>({
+        name: "Alex",
+        age: 24
+    })
+
+    directUpdate(){
+      this.numbers.set([100,200,300]);
+    }
+
+    addNumber(){
+        this.numbers.update(arr => [...arr,400])
+    }
 }
