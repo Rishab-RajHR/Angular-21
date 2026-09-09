6d0c1366117b81bd9b845c38fc7e636bbe25cf1c
Rishab Raj <rr0948555@gmail.com>
2026-09-09 12:39:48 +0530

    Computed Signal

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,30 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, signal, computed } from '@angular/core';
 
 @Component({
-  imports: [RouterOutlet],
+  imports: [],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
 })
 export class App {
-  protected readonly title = signal('angular-tut');
-  name = "Angular Tutorial";
-  username = "TovinoThomas";
-   isAdmin = true;
-   count = 3;
+    //  count = signal<number>(2);
+
+    //  doubleCount = computed<number>(() => this.count() * 2);
+    //  tripleCount = computed<number>(() => this.count() * 3);
+
+    //  increment() {
+    //     this.count.set(this.count() + 1);
+    //  }
+
+    // firstName = signal<string>('Alex');
+    // lastName = signal<string>('Pandian');
+
+    // fullName = computed<string>(() => `${this.firstName()} ${this.lastName()}`);
+
+
+    price = signal<number[]>([10, 20, 30, 40, 50]);
+
+    total = computed<number>(() => {
+        return this.price().reduce((acc, curr) => acc + curr, 0);
+    });
 }
