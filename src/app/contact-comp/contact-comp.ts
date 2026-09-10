0518a6be8f17fcb02026fe13a2644a0627badfcb
Rishab Raj <rr0948555@gmail.com>
2026-09-10 22:46:54 +0530

    Routing

---
+++ src/app/contact-comp/contact-comp.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-contact-comp',
+  styleUrl: './contact-comp.css',
+  templateUrl: './contact-comp.html',
+})
+export class ContactComp {}
---
+++ src/app/home-comp/home-comp.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-home-comp',
+  styleUrl: './home-comp.css',
+  templateUrl: './home-comp.html',
+})
+export class HomeComp {}
