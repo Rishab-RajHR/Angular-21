896da21bac553b91479d1e4968fee1dd27ae9801
Rishab Raj <rr0948555@gmail.com>
2026-09-14 14:50:07 +0530

    Form Submit Post API

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { UserService } from './services/user-service';
 import { User } from './models/user';
@@ -10,7 +10,39 @@ import { User } from './models/user';
   templateUrl: './app.html',
 })
 export class App {
-     userService = inject(UserService);
+    //  userService = inject(UserService);
 
-     users : any = toSignal<User[]>( this.userService.getUsers());
+    //  users : any = toSignal<User[]>( this.userService.getUsers());
+
+    // users: User[] = [];
+
+    users = signal<User[]>([]);
+    name = signal<string>('');
+    email = signal<string>('');
+
+    constructor( private userService: UserService ){}
+
+    ngOnInit(){
+        this.loadUsers();  // Load users on component initialization
+    }
+
+    loadUsers(){
+       this.userService.getUsers().subscribe(data => {
+          this.users.set(data);
+       });
+    }
+
+    submitForm(){
+      const payload: User = {
+         name: this.name(),
+         email: this.email(),
+         isActive: false
+      };
+      this.userService.addUser(payload).subscribe(() => {
+          alert('User added successfully');
+          this.loadUsers();   // Refresh the user list after adding a new user
+          this.name.set('');
+          this.email.set('');
+      })
+    }
 }
