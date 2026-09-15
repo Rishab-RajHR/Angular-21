277a38827d76332df1023f8a137dde7819fc34bb
Rishab Raj <rr0948555@gmail.com>
2026-09-15 10:30:18 +0530

    Bootstrap

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,5 @@
 import { Component, inject, signal } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
-import { UserService } from './services/user-service';
-import { User } from './models/user';
-import { isActive } from '@angular/router';
 
 @Component({
   imports: [],
@@ -11,91 +8,9 @@ import { isActive } from '@angular/router';
   templateUrl: './app.html',
 })
 export class App {
-    //  userService = inject(UserService);
+   name = signal('Alex Pandian');
 
-    //  users : any = toSignal<User[]>( this.userService.getUsers());
-
-    // users: User[] = [];
-
-    users = signal<User[]>([]);
-    name = signal<string>('');
-    email = signal<string>('');
-
-    editingUpdateID = signal<number | null>(null);
-
-    constructor( private userService: UserService ){}
-
-    ngOnInit(){
-        this.loadUsers();  // Load users on component initialization
-    }
-
-    loadUsers(){
-       this.userService.getUsers().subscribe(data => {
-          this.users.set(data);
-       });
-    }
-
-    // Clicked Edit
-    editUser(user: User){
-       this.editingUpdateID.set(user.id!);
-       this.name.set(user.name);
-       this.email.set(user.email);
-    }
-
-    // Add and update User
-    submitForm(){
-      const payload: User = {
-         name: this.name(),
-         email: this.email(),
-         isActive: false
-      };
-      //Update User
-      if(this.editingUpdateID() !== null){
-          this.userService.updateUser(
-            this.editingUpdateID()!,
-            payload
-          ).subscribe(() => {
-              alert('User updated successfuly');
-              this.loadUsers();
-              this.name.set('');
-              this.email.set('');
-              this.editingUpdateID.set(null);
-          })
-      } else {
-         // Add User
-          this.userService.addUser(payload).subscribe(() => {
-          alert('User added successfully');
-          this.afterSave();
-      })
-     }
-    }
-
-    toggleStatus(user: User) {
-       this.userService.updateUserStatus(user.id!, !user.isActive).subscribe(() => {
-          this.users.update(list =>
-             list.map(u =>
-               u.id === user.id ? { ...u, isActive: !u.isActive } : u
-             )
-          )
-       })
-    }
-
-    // Delete a User
-    deleteUser( user: User){
-        const confirmDelete = confirm(`Are you sure you want to delete ${user.name}?`);
-        if( !confirmDelete ) return;
-
-        this.userService.deleteuser(user.id!).subscribe(()=>{
-            this.users.update( list =>
-                list.filter( u => u.id !== user.id )
-            )
-        })
-    }
-
-    afterSave(){
-          this.loadUsers();
-          this.name.set('');
-          this.email.set('');
-          this.editingUpdateID.set(null);
-    }
+   submitForm(){
+      alert(`User ${this.name()} saved successfully!`);
+   }
 }
--- src/app/models/user.ts
+++
@@ -1,6 +0,0 @@
-export interface User{
-    id?: number; // Auto generated ID
-    name: string;
-    email: string;
-    isActive: boolean;
-}
